70bad2869e27ff07816f51af1a3b5b3e24def19a
szejiancheng <sze2001jc@gmail.com>
2024-07-07 00:54:07 +0800

    changes to app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, render_template, send_from_directory, redirect, url_for
 import requests
 from werkzeug.utils import secure_filename
+from FatSecretAPI import fetch_calorie_data
+from HuggingFaceAPI import fetch_labels
 
 
 ## Create flask app
@@ -11,35 +13,18 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 
-## Functions
+## Helper functions
 ## Checks filename and type
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-## Run inference on HuggingFace
-API_URL = "https://api-inference.huggingface.co/models/nateraw/food"
-headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
-
-def query(filename):
-    file_path = app.config['UPLOAD_FOLDER'] + '/' + filename
-    with open(file_path, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    ## TODO: Handle errors from HuggingFace
-    return response.json() ## Reponse should be image labels and scores
-
-
-## Get nutrition info from FatSecret
-def query_fs(food_label):
-
+def make_API_call_to_huggingface(image):
     return
 
-## Routes
-## Photo upload
-@app.route('/')
-def upload():
-    return render_template('upload.html')
+def make_API_call_to_fatsecret(labels):
+    return fetch_calorie_data(labels)
+     
 
 ## Handles photo upload
 @app.route('/upload', methods=['POST'])
@@ -58,15 +43,43 @@ def upload_file():
     else:
         return jsonify({'error': 'File type not allowed'}), 400
     
-## Serve uploaded photo
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    output = query(filename)
-    ## TODO: For each label, get nutrition info
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename), \
-        jsonify({'message': 'File successfully uploaded', 'output': output}), 200
+@app.route('/fats', methods=['POST'])  # Ensure the method is set to accept POST requests
+def test_make_API_call_to_fatsecret():
+    data = request.get_json()  # Parse JSON data from the request body
+    if not data or 'labels' not in data:  # Check if data is parsed and 'labels' key exists
+        return jsonify({'error': 'No labels found'}), 400
 
+    res =  fetch_calorie_data(data['labels'])
+    print(res)
+    return jsonify(res), 200
 
+@app.route('/hug', methods=['POST'])  # Ensure the method is set to accept POST requests
+def test_make_API_call_to_hug():
+    data = request.data
+    if not data: 
+        return jsonify({'error': 'No image found'}), 400
+    res = fetch_labels(data)
+    return jsonify(res), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/infer', methods=['POST'])  # Ensure the method is set to accept POST requests
+def make_combined_requests():
+    print('infer called')
+    data = request.data
+    if not data: 
+        return jsonify({'error': 'No image found'}), 400
+    print('fetching labels...\n')
+    labelsResponse = fetch_labels(data)
+    print(labelsResponse)
+    labels = labelsResponse
+    label_array = []
+    for label in labels:
+        if 'label' in label:
+            label_array.append(label['label'])
+    print('fetching calorie data...\n')
+    res = fetch_calorie_data(label_array)
+    for i in range(len(res)):
+        if res[i] is not None and 'food_name' in res[i]:
+            labels[i]['label'] = res[i]['food_name']
+            labels[i]['calories'] = res[i]['calorie_count']
+            labels[i]['portion_size'] = res[i]['portion_size']
+    return jsonify(labels), 200
